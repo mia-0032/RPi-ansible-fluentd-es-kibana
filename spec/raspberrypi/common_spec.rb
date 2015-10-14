4618446bc6d99d94dae2ee9fa2c8fd35907678da
Yoshihiro MIYAI <msparrow17@gmail.com>
2015-10-14 22:06:08 +0900

    Add ntp test.

--- spec/raspberrypi/common_spec.rb
+++ spec/raspberrypi/common_spec.rb
@@ -14,3 +14,23 @@ describe file('.profile') do
   let(:disable_sudo) { true }
   its(:content) { should match /export LANG=ja_JP\.UTF-8/ }
 end
+
+describe 'Linux kernel parameters' do
+  context linux_kernel_parameter('fs.file-max') do
+    its(:value) { should eq 794573 }
+  end
+end
+
+describe process("ntpd") do
+  it { should be_running }
+end
+
+describe service('ntp') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe command('ntpq -p') do
+  let(:disable_sudo) { true }
+  its(:stdout) { should match /\.nict\.go\./ }
+end
